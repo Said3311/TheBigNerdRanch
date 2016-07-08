6e56fe309e05acb66a713f68530f7cb12fff91cc
Said3311 <said.shaykhalov@gmail.com>
2016-07-08 18:02:52 +0300

    First Changes
    111

--- MyApplication2/app/src/main/java/com/example/said/geoquiz/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/said/geoquiz/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.said.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.quiz, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id==R.id.action_settings)
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
